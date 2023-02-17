e0c81089cd6d37e1c613159015f0d53d39190042
Mihkel Tiganik <mtiganik@gmail.com>
2023-02-18 01:56:54 +0200

    working!!!

--- src/ex-8.js
+++ src/ex-8.js
@@ -4,33 +4,35 @@ import React from 'react';
 // React is loaded and is available as React and ReactDOM
 // imports should NOT be used
 const CommentList = (props) => {
+
+const [currentVal, setCurrentVal] = React.useState("");
+const [valuesList, setValuesList] = React.useState([])
+
+function handleClick(){
+    if(currentVal !== "" && currentVal !== null){
+        setValuesList([...valuesList, currentVal]);
+        setCurrentVal("")
+    }
+
+}
     return (<div>
         <form>
-          <input type="text" />
-          <input type="button" value="Post" />
+          <input type="text" onChange={(event) => setCurrentVal(event.target.value)} value = {currentVal}/>
+          <input type="button" value="Post" onClick={handleClick}/>
         </form>
         <ul>
+            {
+                valuesList.map((item,i) =>
+                
+                    // {console.log(item)}
+                    <li key={i}>{item}</li>
+                )
+            }
         </ul>
       </div>);
   }
   
 
   export default CommentList;
-//   document.body.innerHTML = "<div id='root'></div>";
+
   
-//   const root = ReactDOM.createRoot(document.getElementById("root"));
-//   root.render(<CommentList />);
-  
-//   setTimeout(() => {
-//     var input = document.querySelector("input[type='text']");
-//     input.value = "test";
-//     input._valueTracker.setValue("");
-//     input.dispatchEvent(new Event('change', { bubbles: true }));
-  
-//     setTimeout(() => {
-//       document.querySelector("input[type='button']").click();
-//       setTimeout(() => {
-//         console.log(document.getElementsByTagName("ul")[0].innerHTML);
-//       });
-//     });
-//   });
